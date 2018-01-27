c5d0d89211f42b96e1c98b0ff8b85bf372085415
Duboviy91 <romario_08PR@mail.ru>
2018-01-27 10:48:23 +0300

    migrate to gradle build system

--- build.sh
+++
@@ -1,7 +0,0 @@
-export JAVA_HOME="/cygdrive/c/Program Files/Java/jdk-9.0.1/"
-export PATH="$PATH:$JAVA_HOME/bin"
-
-rm -rf ./bin
-mkdir -p bin
-
-javac -d bin -sourcepath src src/org/pribyshchuk/ttt/TickTackToeMain.java
